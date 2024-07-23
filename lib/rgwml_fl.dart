9a54d6d67e5ccbb322c8b4fd0aec400bc738a1e7
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-07-23 06:26:44 +0530

    sync

--- lib/rgwml_fl.dart
+++ lib/rgwml_fl.dart
@@ -1,4 +1,5 @@
 library rgwml_fl;
 
+export 'package:flutter/material.dart';
 export 'src/widgets/dashboard.dart';
 
--- lib/src/widgets/dashboard.dart
+++ lib/src/widgets/dashboard.dart
@@ -1,39 +1,61 @@
 import 'package:flutter/material.dart';
 
 class CRMDashboard extends StatelessWidget {
+  final String title;
+  final String username;
+  final String userId;
   final List<String> cardTitles;
 
-  CRMDashboard({required this.cardTitles});
+  CRMDashboard({
+    required this.title,
+    required this.username,
+    required this.userId,
+    required this.cardTitles,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text('CRM Dashboard'),
+        title: Text(title),
         backgroundColor: Colors.black,
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            childAspectRatio: 3 / 2,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-          ),
-          itemCount: cardTitles.length,
-          itemBuilder: (context, index) {
-            return Card(
-              color: Colors.grey[850],
-              child: Center(
-                child: Text(
-                  cardTitles[index],
-                  style: TextStyle(color: Colors.white, fontSize: 18),
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                'Welcome, $username (ID: $userId)',
+                style: TextStyle(color: Colors.white, fontSize: 18),
               ),
-            );
-          },
+            ),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: cardTitles.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Colors.grey[850],
+                    child: Center(
+                      child: Text(
+                        cardTitles[index],
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
         ),
       ),
     );
