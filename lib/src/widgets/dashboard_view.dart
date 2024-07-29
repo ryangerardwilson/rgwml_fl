aa10120237341a05b61e548e99de980fa94dea17
Ryan Gerard Wilson <ryangerardwilson@gmail.com>
2024-07-29 17:28:55 +0530

    sync

--- lib/src/widgets/dashboard_view.dart
+++ lib/src/widgets/dashboard_view.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dynamic_table.dart';  // Ensure you have the correct import path
 import '../modal_config.dart'; // Ensure the correct path
 
-class DashboardView extends StatelessWidget {
+class DashboardView extends StatefulWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
   final String title;
   final String username;
@@ -34,22 +34,29 @@ class DashboardView extends StatelessWidget {
   });
 
   @override
+  _DashboardViewState createState() => _DashboardViewState();
+}
+
+class _DashboardViewState extends State<DashboardView> {
+  Map<String, String?> _selectedReadRoute = {};
+
+  @override
   Widget build(BuildContext context) {
-    final modalKeys = modalConfig.configs.keys.toList();
+    final modalKeys = widget.modalConfig.configs.keys.toList();
 
     return Scaffold(
-      key: scaffoldKey,
+      key: widget.scaffoldKey,
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text(
-          title,
+          widget.title,
           style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.black,
         actions: [
           IconButton(
             icon: Icon(Icons.settings, color: Colors.white),
-            onPressed: () => onSettingsPressed(context),
+            onPressed: () => widget.onSettingsPressed(context),
           ),
         ],
       ),
@@ -63,35 +70,22 @@ class DashboardView extends StatelessWidget {
                 itemCount: modalKeys.length,
                 itemBuilder: (context, index) {
                   final modalKey = modalKeys[index];
-                  final modal = modalConfig.configs[modalKey];
+                  final modal = widget.modalConfig.configs[modalKey];
 
                   if (modal != null) {
-                    final readRoute = modal.readRoutes.isNotEmpty
-                        ? modal.readRoutes[0]
-                        : '';
-                    final readFields = modal.scopes.read;
-
                     return GestureDetector(
                       onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => Scaffold(
-                              appBar: AppBar(
-                                title: Text(
-                                  modalKey,
-                                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-                                ),
-                                backgroundColor: Colors.black,
-                              ),
-                              body: DynamicTable(
-                                apiHost: apiHost,
-                                modal: modalKey,
-                                route: readRoute,
-                                readFields: readFields,
-                              ),
-                            ),
-                          ),
+                        setState(() {
+                          _selectedReadRoute[modalKey] = modal.readRoutes.isNotEmpty
+                              ? modal.readRoutes[0]
+                              : null;
+                        });
+
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return _buildRouteSelectionDialog(context, modalKey, modal.readRoutes);
+                          },
                         );
                       },
                       child: Card(
@@ -104,16 +98,12 @@ class DashboardView extends StatelessWidget {
                             children: [
                               Text(
                                 modalKey,
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 18),
+                                style: TextStyle(color: Colors.white, fontSize: 18),
                               ),
                               SizedBox(height: 10),
                               Text(
                                 'Tap to view details',
-                                style: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 14,
-                                ),
+                                style: TextStyle(color: Colors.white70, fontSize: 14),
                               ),
                             ],
                           ),
@@ -121,7 +111,7 @@ class DashboardView extends StatelessWidget {
                       ),
                     );
                   } else {
-                    return SizedBox.shrink(); // Return an empty widget if modal is null
+                    return SizedBox.shrink();
                   }
                 },
               ),
@@ -131,5 +121,68 @@ class DashboardView extends StatelessWidget {
       ),
     );
   }
+
+Widget _buildRouteSelectionDialog(BuildContext context, String modalKey, List<String> routes) {
+  return AlertDialog(
+    backgroundColor: Colors.black,  // Set dialog background color
+    title: Text(
+      'Select filter for $modalKey',
+      style: TextStyle(color: Colors.white),
+    ),
+    content: Container(
+      width: double.maxFinite,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: routes.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[850],  // Set button background color
+              ),
+              onPressed: () {
+                setState(() {
+                  _selectedReadRoute[modalKey] = routes[index];
+                });
+                Navigator.of(context).pop();
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Scaffold(
+                      appBar: AppBar(
+                        title: Text(
+                          modalKey,
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        backgroundColor: Colors.black,
+                      ),
+                      body: DynamicTable(
+                        apiHost: widget.apiHost,
+                        modal: modalKey,
+                        route: _selectedReadRoute[modalKey] ?? '',
+                        readFields: widget.modalConfig.configs[modalKey]!.scopes.read,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              child: Text(
+                routes[index],
+                style: TextStyle(color: Colors.white),  // Set text color to white
+              ),
+            ),
+          );
+        },
+      ),
+    ),
+  );
+}
+
+
 }
 
--- lib/src/widgets/dynamic_table.dart
+++ lib/src/widgets/dynamic_table.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+
 
 // Define a function to fetch data from the API
 Future<List<Map<String, dynamic>>> fetchData(String apiHost, String modal, String route) async {
@@ -37,9 +39,7 @@ Future<List<Map<String, dynamic>>> fetchData(String apiHost, String modal, Strin
   }
 }
 
-
-
-class DynamicTable extends StatelessWidget {
+class DynamicTable extends StatefulWidget {
   final String apiHost;
   final String modal;
   final String route;
@@ -53,68 +53,248 @@ class DynamicTable extends StatelessWidget {
   });
 
   @override
+  _DynamicTableState createState() => _DynamicTableState();
+}
+
+class _DynamicTableState extends State<DynamicTable> {
+  List<Map<String, dynamic>> _searchData = [];
+  String? _queryError;
+
+  Future<void> handleSearchSubmit(String apiHost, String modal, String queryInput) async {
+    final fullUrl = apiHost + 'search/$modal';
+    print('Requesting URL: $fullUrl');
+
+    final payload = jsonEncode({'search_string': queryInput.trim()});
+    print('Payload: $payload');
+
+    try {
+      final response = await http.post(
+        Uri.parse(fullUrl),
+        headers: {'Content-Type': 'application/json'},
+        body: payload,
+      );
+
+      print('Full Response: ${response.body}');
+
+      if (response.statusCode == 200) {
+        final result = jsonDecode(response.body);
+        List<String> columns = List<String>.from(result["columns"]);
+        List<List<dynamic>> data = List<List<dynamic>>.from(result["data"]);
+        List<Map<String, dynamic>> dataList = data.map((row) {
+          return Map<String, dynamic>.fromIterables(columns, row);
+        }).toList();
+
+        setState(() {
+          _searchData = dataList;
+          _queryError = null;
+        });
+      } else {
+        print('Error fetching search results: ${response.body}');
+        setState(() {
+          _queryError = 'Error fetching search results: ${response.reasonPhrase}';
+        });
+      }
+    } catch (error) {
+      print('Error fetching search results: $error');
+      setState(() {
+        _queryError = error.toString();
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black,  // Set background color to black
-      child: FutureBuilder<List<Map<String, dynamic>>>(
-        future: fetchData(apiHost, modal, route),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}', style: TextStyle(color: Colors.white));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return Text('No data available', style: TextStyle(color: Colors.white));
-          } else {
-            final data = snapshot.data!;
-            return ListView.builder(
-              shrinkWrap: true,
-              physics: NeverScrollableScrollPhysics(),  // Disable ListView scrolling
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                  onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (context) => _buildScrollableDialog(context, data[index]),
-                    );
-                  },
-                  child: Card(
-                    color: Colors.grey[850],
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          for (int i = 0; i < readFields.length; i++)
-                            if (i < 5 || i >= readFields.length - 2)
-                              Text(
-                                '${readFields[i]}: ${data[index][readFields[i]]}',
-                                style: TextStyle(color: Colors.white),
-                              ),
-                        ],
-                      ),
+    return Scaffold(
+      body: Container(
+        color: Colors.black, // Set background color to black
+        child: FutureBuilder<List<Map<String, dynamic>>>(
+          future: fetchData(widget.apiHost, widget.modal, widget.route),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  'Error: ${snapshot.error}',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return Center(
+                child: Text(
+                  'No data available',
+                  style: TextStyle(color: Colors.white),
+                  textAlign: TextAlign.center,
+                ),
+              );
+            } else {
+              final data = _searchData.isNotEmpty ? _searchData : snapshot.data!;
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Filter: ${widget.route}', // Display the name of the route
+                      style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
                   ),
-                );
-              },
-            );
-          }
-        },
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'Rows Fetched: ${data.length}', // Display the count of data rows
+                      style: TextStyle(color: Colors.white, fontSize: 14),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: data.length,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => _buildScrollableDialog(context, data[index]),
+                            );
+                          },
+                          child: Card(
+                            color: Colors.grey[850],
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  for (int i = 0; i < widget.readFields.length; i++)
+                                    if (i < 5 || i >= widget.readFields.length - 2)
+                                      Text(
+                                        '${widget.readFields[i]}: ${data[index][widget.readFields[i]]}',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              String queryInput = '';
+              return AlertDialog(
+                title: Text('Search', style: TextStyle(color: Colors.white)),
+                backgroundColor: Colors.black,
+                content: TextField(
+                  onChanged: (value) {
+                    queryInput = value;
+                  },
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Enter search query',
+                    hintStyle: TextStyle(color: Colors.white60),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.blue),
+                    ),
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    child: Text('Cancel', style: TextStyle(color: Colors.white)),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: Text('Search', style: TextStyle(color: Colors.white)),
+                    onPressed: () {
+                      handleSearchSubmit(widget.apiHost, widget.modal, queryInput);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: Icon(Icons.search, color: Colors.white),
+        backgroundColor: Colors.black,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(color: Colors.white, width: 2.0),
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      bottomNavigationBar: _queryError != null ? BottomAppBar(
+        child: Text(_queryError!, style: TextStyle(color: Colors.red)),
+      ) : null,
     );
+  }
+
+  Future<void> _launchInBrowser(String url, BuildContext context) async {
+    final Uri uri = Uri.parse(url);
+    if (!await launchUrl(uri, mode: LaunchMode.externalApplication)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Could not launch $url')),
+      );
+    }
   }
 
   Widget _buildScrollableDialog(BuildContext context, Map<String, dynamic> item) {
     return AlertDialog(
-      backgroundColor: Colors.black,  // Set the dialog background to black
+      backgroundColor: Colors.black, // Set the dialog background to black
       content: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: item.entries.map((entry) => Text(
-            '${entry.key}: ${entry.value}',
-            style: TextStyle(color: Colors.white),  // Set text color to white
-          )).toList(),
+          children: item.entries.map((entry) {
+            if (Uri.tryParse(entry.value.toString()) != null && Uri.parse(entry.value.toString()).isAbsolute) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${entry.key}: ${entry.value}',
+                    style: TextStyle(color: Colors.white), // Set text color to white
+                  ),
+                  SizedBox(height: 4), // Adding some spacing
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.grey, // Set button background color to grey
+                    ),
+                    onPressed: () => _launchInBrowser(entry.value.toString(), context),
+                    child: Text(
+                      'Open URL',
+                      style: TextStyle(color: Colors.white), // Set button text color to white
+                    ),
+                  ),
+                  SizedBox(height: 16), // Adding some spacing after each entry
+                ],
+              );
+            } else {
+              return Padding(
+                padding: const EdgeInsets.only(bottom: 16),
+                child: Text(
+                  '${entry.key}: ${entry.value}',
+                  style: TextStyle(color: Colors.white), // Set text color to white
+                ),
+              );
+            }
+          }).toList(),
         ),
       ),
       actions: [
@@ -122,7 +302,7 @@ class DynamicTable extends StatelessWidget {
           onPressed: () => Navigator.of(context).pop(),
           child: Text(
             'Close',
-            style: TextStyle(color: Colors.white),  // Set text color to white
+            style: TextStyle(color: Colors.white), // Set text color to white
           ),
         ),
       ],
